11e8bf58c7f3f33dd72b751f34a1d8439ec18983
Aakash Shanbhag <aakashshanbhag@Aakashs-Air-68.attlocal.net>
2017-11-13 00:40:33 -0800

    Second Addition

---
+++ ComputerGraphics/Exercise-2-AakashShanbhag/assign2/assign2.cpp
@@ -0,0 +1,124 @@
+/*
+  CSCI 480
+  Assignment 2
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <OpenGL/gl.h>
+#include <OpenGL/glu.h>
+#include <GLUT/glut.h>
+#include "pic.h"
+
+/* represents one control point along the spline */
+struct point {
+   double x;
+   double y;
+   double z;
+};
+
+/* spline struct which contains how many control points, and an array of control points */
+struct spline {
+   int numControlPoints;
+   struct point *points;
+};
+
+/* the spline array */
+struct spline *g_Splines;
+
+/* total number of splines */
+int g_iNumOfSplines;
+
+
+int loadSplines(char *argv) {
+  char *cName = (char *)malloc(128 * sizeof(char));
+  FILE *fileList;
+  FILE *fileSpline;
+  int iType, i = 0, j, iLength;
+
+
+  /* load the track file */
+  fileList = fopen(argv, "r");
+  if (fileList == NULL) {
+    printf ("can't open file\n");
+    exit(1);
+  }
+  
+  /* stores the number of splines in a global variable */
+  fscanf(fileList, "%d", &g_iNumOfSplines);
+
+  g_Splines = (struct spline *)malloc(g_iNumOfSplines * sizeof(struct spline));
+
+  /* reads through the spline files */
+  for (j = 0; j < g_iNumOfSplines; j++) {
+    i = 0;
+    fscanf(fileList, "%s", cName);
+    fileSpline = fopen(cName, "r");
+
+    if (fileSpline == NULL) {
+      printf ("can't open file\n");
+      exit(1);
+    }
+
+    /* gets length for spline file */
+    fscanf(fileSpline, "%d %d", &iLength, &iType);
+
+    /* allocate memory for all the points */
+    g_Splines[j].points = (struct point *)malloc(iLength * sizeof(struct point));
+    g_Splines[j].numControlPoints = iLength;
+
+    /* saves the data to the struct */
+    while (fscanf(fileSpline, "%lf %lf %lf", 
+	   &g_Splines[j].points[i].x, 
+	   &g_Splines[j].points[i].y, 
+	   &g_Splines[j].points[i].z) != EOF) {
+      i++;
+    }
+  }
+
+  free(cName);
+
+  return 0;
+}
+
+
+int main (int argc, char ** argv)
+{
+  if (argc<2)
+  {  
+  printf ("usage: %s <trackfile>\n", argv[0]);
+  exit(0);
+  }
+
+  loadSplines(argv[1]);
+  
+  	/* initialization */
+  	glutInit(&argc, (char**)argv);
+	glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB | GLUT_DEPTH);
+
+	/* create window */
+	glutInitWindowSize(640, 480);
+	glutInitWindowPosition(100, 100);
+	glutCreateWindow("CSCI 420 Assignment 2 - Rollercoaster");
+
+	/* used for double buffering */
+	glEnable(GL_DEPTH_TEST);
+
+	/* tells glut to use a particular display function to redraw */
+	glutDisplayFunc(display);
+	glutReshapeFunc(reshape);
+
+	/* replace with any animate code */
+	glutIdleFunc(idle);
+
+	/* user keyboard input */
+	glutSpecialFunc(keySpecial);
+
+	/* enable materials */
+
+	myInit();
+
+	glutMainLoop();
+
+  return 0;
+}
